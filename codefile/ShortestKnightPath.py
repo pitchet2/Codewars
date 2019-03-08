e7b87ed72cc0adea456b6fa00f56c143fc612d33
pitchet2 <45702149+pitchet2@users.noreply.github.com>
2019-03-08 16:30:09 -0700

    Update ShortestKnightPath.py

--- codefile/ShortestKnightPath.py
+++ codefile/ShortestKnightPath.py
@@ -1,5 +1,4 @@
-#Given two different positions on a chess board, find the least number of moves it would take a knight to get from one to the other.
-# 
+
 # Given two different positions on a chess board, find the least number of moves it would take a knight to get from one to the other. The positions will be passed as two arguments in algebraic notation. For example, knight("a3", "b5") should return 1.
 # 
 # The knight is not allowed to move off the board. The board is 8x8.
